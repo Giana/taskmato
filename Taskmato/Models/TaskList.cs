ba2264d5f749e766ef7da877abfd657c7780b59e
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2020-11-10 22:06:54 -0500

    Created Task and TaskList class skeletons

---
+++ Taskmato/Models/TaskList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Taskmato.Models
+{
+    public class TaskList
+    {
+        private DateTime Date;
+        private List<Task> Tasks;
+
+        public DateTime GetDate() { return Date; }
+
+        public void SetDate(DateTime date) { Date = date; }
+
+        public List<Task> GetTasks() { return Tasks; }
+
+        public void SetTasks(List<Task> tasks) { Tasks = tasks; }
+
+        public void AddTask(Task task)
+        {
+            // Steven's code
+        }
+
+        public void DeleteTask(Task task)
+        {
+            // Mason's code
+        }
+    }
+}
